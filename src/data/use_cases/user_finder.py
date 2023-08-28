c0a9dcb32c70dc45aa469894d829364d3a11fe8a
alfmorais <alfredneto@hotmail.com>
2023-08-28 16:59:41 -0300

    feat: implement use_cases testing

--- src/data/use_cases/user_finder.py
+++ src/data/use_cases/user_finder.py
@@ -1,3 +1,4 @@
+#pylint: disable=broad-exception-raised
 from typing import Dict
 from src.domain.use_cases.users_finder import UserFinderInterface
 from src.data.interfaces.users_repositories import UsersRepositoryInterface
@@ -8,4 +9,28 @@ class UserFinder(UserFinderInterface):
         self.__users_repository = users_repository
 
     def find(self, first_name: str) -> Dict:
-        return self.__users_repository.select_user(first_name)
+        """
+        Regras:
+        
+        - first_name contendo números
+        - first_name com mais de 18 letras
+        - retornar erro em caso não encontre o usuário
+        - formatar a resposta
+        """
+        if not first_name.isalpha():
+            raise Exception("Nome invalido para a busca")
+
+        if len(first_name) > 18:
+            raise Exception("Nome muito grande para a busca")
+
+        users = self.__users_repository.select_user(first_name)
+
+        if users == []:
+            raise Exception("Usuario não encontrado")
+
+        response = {
+            "type": "Users",
+            "count": len(users),
+            "attributes": users,
+        }
+        return response
--- src/data/use_cases/user_finder_test.py
+++ src/data/use_cases/user_finder_test.py
@@ -1,9 +1,48 @@
-from src.infra.db.repositories.users_repositories import UsersRepository
+import pytest
+from src.infra.db.tests.users_repositories import UsersRepositorySpy
 from .user_finder import UserFinder
 
 
-def test_user_finder():
-    users_repository = UsersRepository()
+def test_user_finder_error_first_name_is_not_alpha():
+    first_name = "100 Asilo"
+    users_repository = UsersRepositorySpy()
     user_finder = UserFinder(users_repository)
 
-    assert user_finder is not None
+    with pytest.raises(Exception) as error:
+        user_finder.find(first_name)
+
+    assert error.value.args[0] == "Nome invalido para a busca"
+
+
+def test_user_finder_error_first_name_len_more_than_max_characters():
+    first_name = "RedWackyLeagueAntlezBroketheStereoNeon"
+    users_repository = UsersRepositorySpy()
+    user_finder = UserFinder(users_repository)
+
+    with pytest.raises(Exception) as error:
+        user_finder.find(first_name)
+
+    assert error.value.args[0] == "Nome muito grande para a busca"
+
+
+def test_user_finder_error_first_name_not_found():
+    first_name = "Davi"
+    users_repository = UsersRepositorySpy()
+    user_finder = UserFinder(users_repository)
+
+    with pytest.raises(Exception) as error:
+        user_finder.find(first_name)
+
+    assert error.value.args[0] == "Usuario não encontrado"
+
+
+def test_user_finder_success():
+    first_name = "Joaquim"
+    users_repository = UsersRepositorySpy()
+    user_finder = UserFinder(users_repository)
+
+    response = user_finder.find(first_name)
+
+    assert response["type"] == "Users"
+    assert response["count"] == 2
+    assert response["attributes"] != []
