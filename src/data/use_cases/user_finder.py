1a1056422fcb83ad6af1541c51a7830214f7e3e5
alfmorais <alfredneto@hotmail.com>
2023-08-29 10:17:16 -0300

    refactor: class UserFinder to understand better concepts about OOP

--- src/data/use_cases/user_finder.py
+++ src/data/use_cases/user_finder.py
@@ -1,5 +1,5 @@
 #pylint: disable=broad-exception-raised
-from typing import Dict
+from typing import Dict, List
 from src.domain.use_cases.users_finder import UserFinderInterface
 from src.data.interfaces.users_repositories import UsersRepositoryInterface
 
@@ -9,28 +9,42 @@ class UserFinder(UserFinderInterface):
         self.__users_repository = users_repository
 
     def find(self, first_name: str) -> Dict:
-        """
-        Regras:
-        
-        - first_name contendo números
-        - first_name com mais de 18 letras
-        - retornar erro em caso não encontre o usuário
-        - formatar a resposta
-        """
+        self.__validate_name(first_name)
+        users = self.__search_user(first_name)
+        return self.__format_response(users)
+
+    @classmethod
+    def __validate_name(cls, first_name: str) -> None:
         if not first_name.isalpha():
             raise Exception("Nome invalido para a busca")
 
         if len(first_name) > 18:
             raise Exception("Nome muito grande para a busca")
 
+    def __search_user(self, first_name: str) -> List:
         users = self.__users_repository.select_user(first_name)
 
         if users == []:
             raise Exception("Usuario não encontrado")
 
+        return users
+
+    @classmethod
+    def __format_response(cls, users: list) -> Dict:
+        attributes = []
+
+        for user in users:
+            attributes.append(
+                {
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                    "age": user.age,
+                }
+            )
+
         response = {
             "type": "Users",
-            "count": len(users),
-            "attributes": users,
+            "count": len(attributes),
+            "attributes": attributes,
         }
         return response
